a20e8bf755e93367523a12802da675678efad0bb
Wanqing Liu <wanqingliu723@gmail.com>
2022-06-14 13:50:20 -0400

    followed the to do list iteration guideline

---
+++ kwk To-Do List Project/toDO.swift
@@ -0,0 +1,14 @@
+//
+//  toDO.swift
+//  toDoList.project
+//
+//  Created by aliu.22@desales.co on 6/14/22.
+//
+
+import UIKit
+
+class toDo {
+    var name = ""
+    var important = false
+    
+}
